8a7982de1aa727dadb3b08615c84630e01cc9101
Yuliia Khrupina <julichka1391@gmail.com>
2022-04-19 21:06:55 -0700

    added list item

---
+++ Multithreading/ListItem.swift
@@ -0,0 +1,18 @@
+//
+//  ListItem.swift
+//  Multithreading
+//
+//  Created by Yuliia Khrupina on 4/19/22.
+//
+
+import Foundation
+import UIKit
+
+class ListItem : UITableViewCell {
+
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+}
